f4f02dcc772f14aaa6a82b925774c317b3afceb7
Sarthak <sarthak.whenever@gmail.com>
2024-02-12 01:01:32 +0545

    professional ref added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Nav from './Components/Nav';
+import { Chip, Typography } from "@mui/material";
+import Link from "next/link";
+import { ContactPage } from "@mui/icons-material";
 export default function Home() {
   return (
     <>
-    <Nav/>
-    <main>
-      
+      <Nav />
+      <main>
 
-    </main>
+        <div style={{position:'absolute', left:'50%', transform:'translateX(-50%)',bottom: '25%'}}>
+        <Typography variant='h5' style={{textAlign:'center'}}>Looking for my &nbsp;
+          <Link target="_blank" href="https://prof.sarthakpokhrel.com.np" style={{textDecoration:'none', color:'#00e'}}>
+            Professional Portfolio
+          </Link> ?
+        </Typography>
+        </div>
+      </main>
     </>
   );
 }
