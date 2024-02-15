14df307e0ae57c254483a7458b35cbb1897ed9c9
Sarthak <sarthak.whenever@gmail.com>
2024-02-15 18:50:15 +0545

    trashdump ready

--- src/app/trashdump/page.tsx
+++ src/app/trashdump/page.tsx
@@ -1,21 +1,112 @@
-import { Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 import Nav from "../Components/Nav";
+import trashtree from '../trashtree.json';
+import { wrap } from "module";
+import timeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+timeAgo.addDefaultLocale(en)
+// import Markdown from "react-markdown";
+trashtree.reverse();
 
-export default function trashdump(){
+const timeAgoParser = new timeAgo('en-US');
+
+
+
+function TCard({ trash }) {
+    if (trash.content.imgs.length == 1) {
+        return <ClassicTCard trash={trash} />
+    }
+    if (!trash.title) {
+        return <TextOnly trash={trash} />
+    }
+}
+
+function ClassicTCard({ trash }) {
+    return (<>
+
+        <div style={{ display: 'flex', flexDirection: 'column', height: "100%", justifyContent: 'space-between' }}>
+            <div>
+                {trash.content.imgs[0] ?
+
+                    <CardMedia image={"https://sarthak-pokharel.github.io/sarthak/[TrashDump]/trashcan.jpg"} sx={{ height: 120 }} title={trash.content.title} /> :
+
+                    ""}
+                <CardContent>
+                    {trash.content.text ? <Typography sx={{ "whiteSpace": "pre-warp" }}>
+                        {trash.content.text}
+                    </Typography> : ""}
+
+
+                </CardContent>
+            </div>
+            <div>
+            <CardBottom trash={trash} />
+            </div>
+        </div>
+
+
+
+    </>);
+}
+function CardBottom({trash}){
+    return <CardContent sx={{ paddingTop: 0,paddingBottom:"0 !important" }}>
+    <Typography variant="body" color="text.secondary" sx={{fontWeight:'bold'}} component="div">
+        {trash.content.title||"random bs"}
+    </Typography>
+    <TimeAgoComp trash={trash}/>
+</CardContent>;
+}
+function TimeAgoComp({trash}){
+    return (<Typography variant="caption" color="text.secondary" sx={{ textAlign:'right', padding:0}} component="div"
+            >{timeAgoParser.format(trash.date)}</Typography>)
+}
+
+
+function TextOnly({ trash }) {
     return <>
-    
-    <Nav/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
-    <div style={{textAlign:'center'}}>
-        <Typography variant="h4">
-            page under contruction.
-        </Typography>
-    </div>
-    
+        <div style={{ display: 'flex', flexDirection: 'column', height: "100%", justifyContent: 'space-between' }}>
+
+            <CardContent>
+                <Typography
+
+                    sx={{ "whiteSpace": "pre-wrap" }}
+                >{trash.content.text}</Typography>
+            </CardContent>
+            <CardBottom trash={trash} />
+        </div>
+    </>
+}
+
+
+
+
+
+export default function trashdump() {
+    return <>
+
+        <Nav />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div style={{ textAlign: 'left', display: 'flex', justifyContent: 'space-evenly', flexWrap: "wrap" }}>
+
+
+            {trashtree.map((trash, i) =>
+                <Card
+                    padding={2}
+                    elevation={0}
+                    sx={{ width: 300, textTransform:'lowercase' }} key={i}
+                >
+
+                    <TCard trash={trash} />
+                </Card>
+
+            )}
+
+        </div>
+
     </>
 }
