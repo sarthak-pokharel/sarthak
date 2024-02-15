2d54200cb38af50b258b5e093f3fef62433e3635
Sarthak <sarthak.whenever@gmail.com>
2024-02-15 23:01:48 +0545

    alt

--- functions/gettrashdumps.tsx
+++ functions/gettrashdumps.tsx
@@ -6,7 +6,12 @@ import {getAllDumpCollection} from './test';
 import 'dotenv/config';
 
 export default async (req: Request, context: Context) => {
-  let col = await getAllDumpCollection();
+  let col;
+  try {
+    col = await getAllDumpCollection();
+  }else {
+    col = []; 
+  }
   return new Response(JSON.stringify(col), { // Sending the data as a response
     headers: { 'Content-Type': 'application/json' },
     status: 200,
