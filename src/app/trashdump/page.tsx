d6c6b51b4014e566ec0234893d96b301c87bda78
Sarthak <sarthak.whenever@gmail.com>
2024-02-16 02:14:36 +0545

    content added

--- src/app/trashdump/page.tsx
+++ src/app/trashdump/page.tsx
@@ -14,7 +14,7 @@ timeAgo.addDefaultLocale(en)
 trashtree.reverse();
 
 async function fetchTrashDumps(){
-    let j = await (await fetch(process.env.URL+'/api/gettrashdumps',{ next: { revalidate: 3600 } })).text();
+    let j = await (await fetch(process.env.URL+'/api/gettrashdumps',{ next: { revalidate: 3600/4 } })).text();
     // console.log("thisiswhatigot",[j])
     try{
         j = JSON.parse(j);
