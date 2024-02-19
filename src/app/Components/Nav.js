a017807450fb6150a8ce9a7be975e76a797c561b
Sarthak <sarthak.whenever@gmail.com>
2024-02-20 00:23:55 +0545

    target

--- src/app/Components/Nav.js
+++ src/app/Components/Nav.js
@@ -10,7 +10,7 @@ export default function Nav(){
     <nav className={styles.nav}>
         <Link className={styles.navroute} href="/">home</Link>
         <Link className={styles.navroute} href="/blogs">blogs</Link>
-        <Link className={styles.navroute} href="/trashdump">trash-dump</Link>
+        <Link target="_blank" className={styles.navroute} href="/trashdump">trash-dump</Link>
         <Link className={styles.navroute} target="_blank" href="https://prof.sarthakpokhrel.com.np">professional</Link>
     </nav>
     </>
