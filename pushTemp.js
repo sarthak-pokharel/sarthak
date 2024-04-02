e0fc9305038aa059655d17ce32a4e8ee7a450181
Sarthak <sarthak.whenever@gmail.com>
2024-04-03 00:46:24 +0545

    content added

--- pushTemp.js
+++ pushTemp.js
@@ -3,17 +3,32 @@ let tcont = {
         "imgs":[],
         "text": 
 `
+and when every last week feels like months ago.
+that when you love it.
+living that is.
+and its like immortality. 
+except its not a curse this time.
+
+add that to being on your own,
+independent of soul crushing expectations from yourself
+independent of poisoned chains of infected souls
+independent of any lethal deluding chemicals
+that sensation of a warm blessing
+every rays that hit your face
+from that rising sun
+
+and when suddenly
+you can hear everything around you
+screaming for attention
+demanding to be seen, explored, admired, felt
+all those abstract complex patterns you've been ignorant
+to smallest, simplest, childish stuffs, fostering joy
+and it all
+just feels right
 
 
-i wont deny it cuz youuu
-saww what it was
-
-i cant deny it if you
-wont give a fk so ill
-sewww it upppp
-
-you know i ammm so in loveee
-
+literally heaven!
+oh how i wish you could experience it
 `.trim(),
         "title": "fireflies - owl city",
         "link": "https://youtu.be/psuRGfAaju4"
