4282f5199976bff49e0a8f222579813b074f1ff0
Sarthak <sarthak.whenever@gmail.com>
2024-04-03 15:09:50 +0545

    blogstylin

--- src/app/blogs/VariableTimeDisplay.tsx
+++ src/app/blogs/VariableTimeDisplay.tsx
@@ -5,9 +5,11 @@ import { Typography } from "@mui/material";
 
 import timeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
+import { TimeAgoComp } from "../trashdump/Dynamics";
 timeAgo.addDefaultLocale(en)
 const timeAgoParser = new timeAgo('en-US');
 
 export default function VariableTimeDisplay({blog}) {
-    return <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right' }} >{timeAgoParser.format(blog.published_date)}</Typography>;
+    // return <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'right' }} >{timeAgoParser.format(blog.published_date)}</Typography>;
+    return <TimeAgoComp trash={{date: blog.published_date}} />
 }
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,7 @@
 import Nav from "../Components/Nav";
 import styles from './blogs.module.css'
 import inf from '../../../[Blogs]/content.json';
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import VariableTimeDisplay from "./VariableTimeDisplay";
 
 
@@ -11,20 +11,23 @@ export default function Blogs() {
     return <>
         <Nav />
 
-        <div className={styles.container}>
-            {inf.blogs.map(blog => <>
 
+        {inf.blogs.map(blog => <>
+            <div className={styles.container}>
                 <div className="blog">
-                    <VariableTimeDisplay blog={blog} />
-                    <Typography sx={{ textTransform: 'lowercase' }} variant="h5">{blog.title}</Typography>
+                    <Typography sx={{ textTransform: 'lowercase', display:'flex', justifyContent:'space-between' }} variant="h5">
+                    <Box>{blog.title}</Box>
+                    <Box><VariableTimeDisplay blog={blog} /></Box>
+                    </Typography>
                     <Typography>{blog.short_description}</Typography>
                     <br />
                     <div>
                         <a href={blog.link} target="_blank" className="read-more">read</a>
                     </div>
                 </div>
-            </>)}
-        </div>
+            </div>
+        </>)}
+
 
     </>
 }
